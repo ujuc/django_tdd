07f32001eec45ea5b11583345321c851065a2f41
Sungjin Kang <ujuc@ujuc.kr>
2015-04-18 15:19:35 +0900

    기능 테스트가 작업 아이템을 추가할 수 있는지 확인

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -20,19 +21,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # 웹페이지 타이틀과 해더가 'To-Do'를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 그녀는 바로 작업을 추가하기로 한다.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            "직업 아이템 입력"
+        )
 
         # "공작깃털 사기"라고 텍스트 상자를 입력한다.
         # (에디스의 취미는 날치 잡이용 그물을 마드는 것이다.)
+        inputbox.send_keys('공작깃털 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다.
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다. (에디스는 매우
         # 체계적인사람이다.)
+        self.fail("Finish the test!")
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
