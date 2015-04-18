9b82a4304e36e71ee547b6de2ec9340a3af7c15a
Sungjin Kang <ujuc@ujuc.kr>
2015-04-18 13:07:51 +0900

    첫 단위 테스트와 URL 맵핑 그리고 임시 뷰

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,10 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEquals(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertAlmostEqual(found.func, home_page)
