6f9bf41f0441317aa0bf68031d7a92d067aafe61
Sungjin Kang <ujuc@ujuc.kr>
2015-04-18 12:47:21 +0900

    첫기능테스트주석추가및unittest 사용

--- functional_test.py
+++ functional_test.py
@@ -1,35 +1,48 @@
 # -*- coding:utf-8 -*-
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# 에디스(Edith)는 멋진 직업 목록 온라인 앱이 나왔다는 소식을 듣고
-# 해당 웹 사이트를 확인하러 간다.
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# 웹페이지 타이틀과 해더가 'To-Do'를 표시하고 있다.
-assert 'To-Do' in brewser.title
+    def setup(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-# 그녀는 바로 작업을 추가하기로 한다.
+    def tearDown(self):
+        self.browser.quit()
 
-# "공작깃털 사기'라고 텍스트 상자를 입력한다.
-# (에디스의 취미는 날치 잡이용 그물을 마드는 것이다.)
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 에디스(Edith)는 멋진 직업 목록 온라인 앱이 나왔다는 소식을 듣고
+        # 해당 웹 사이트를 확인하러 간다.
+        self.browser.get('http://localhost:8000')
 
-# 엔터키를 치면 페이지가 갱신되고 작업 목록에
-# "1: 공작깃털 사기" 아이템이 추가된다.
+        # 웹페이지 타이틀과 해더가 'To-Do'를 표시하고 있다.
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Finish the test!")
 
-# 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
-# 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다. (에디스는 매우 체계적인
-# 사람이다.)
+        # 그녀는 바로 작업을 추가하기로 한다.
 
-# 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
-# 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
-# 사이트는 그녀를 위한 특정 URL을 생성해준다.
-# 이때 URL에 대한 성명도 함께 제공된다.
+        # "공작깃털 사기"라고 텍스트 상자를 입력한다.
+        # (에디스의 취미는 날치 잡이용 그물을 마드는 것이다.)
 
-# 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
+        # "1: 공작깃털 사기" 아이템이 추가된다.
 
-# 만족하고 잠자리에 든다.
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
+        # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다. (에디스는 매우
+        # 체계적인사람이다.)
 
-browser.quit()
+        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
+        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
+        # 사이트는 그녀를 위한 특정 URL을 생성해준다.
+        # 이때 URL에 대한 성명도 함께 제공된다.
+
+        # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
+
+        # 만족하고 잠자리에 든다.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
