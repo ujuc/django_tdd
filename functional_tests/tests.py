7c5d921b726857ef4d424fc9b39a0077f892960f
Sungjin Kang <ujuc@ujuc.kr>
2015-04-22 23:48:34 +0900

    레이아웃과 스타일링을 위한 FT 첫 단계

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -96,3 +96,18 @@ class NewVisitorTest(LiveServerTestCase):
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
 
         # 만족하고 잠자리에 든다.
+
+    def test_layout_and_styling(self):
+        # 에디스는 메인 페이지를 방문한다.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 그녀 새로운 리스트를 시작하고 입력 상자가
+        # 가운데 배치된 것을 확인한다.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing\n')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
