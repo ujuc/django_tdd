3bf1c956f2561723db919eae18f2ec46dd0313cb
Sungjin Kang <ujuc@ujuc.kr>
2015-04-25 02:19:49 +0900

    모델 검증을 위한 신규 목록 뷰 수정

--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
@@ -35,36 +36,6 @@ class HomePageTest(TestCase):
         )
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/{}/'.format(new_list.id))
-
-
-
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_times(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = '첫 번째 아이템'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = '두 번째 아이템'
-        second_item.list = list_
-        second_item.save()
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, '두 번째 아이템')
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -122,3 +93,18 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, '/lists/{}/'.format(correct_list.id))
+
+
+class NewListTest(TestCase):
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
