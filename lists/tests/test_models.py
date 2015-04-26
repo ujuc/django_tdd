5e754b03aea2d4c38101cadff761d0afede42d1a
Sungjin Kang <ujuc@ujuc.kr>
2015-04-27 01:35:06 +0900

    리스트 뷰에서 모델 유효성 검증 구현

---
+++ lists/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from lists.models import Item, List
+from django.core.exceptions import ValidationError
+
+
+class ListAndItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_times(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.list = list_
+        second_item.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list_item(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
