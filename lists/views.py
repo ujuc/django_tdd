ea575fcc5832b63cad25717cbc68f66a450f2da3
Sungjin Kang <ujuc@ujuc.kr>
2015-04-27 23:52:26 +0900

    home_page에서 신규 폼 사용 및 테스트 단순화. 하지만 정상 동작하지 않음.

--- lists/views.py
+++ lists/views.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
