c44bab9e66deb1a9be97b44cfa22d223e9d8ebe1
Sungjin Kang <ujuc@ujuc.kr>
2015-04-29 23:55:36 +0900

    FT 테스트 수정

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,7 +5,7 @@ from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
-    
+
     @skip
     def test_cannot_add_empty_list_items(self):
         # 에디스는 메인 페이지에 접속해서 빈 아이템을 실수로 등록하려한다.
@@ -15,7 +15,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다.
         # 에러 메시지가 표시된다.
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다. ")
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다.
@@ -27,7 +27,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 리스트페이지에 다시 에러 메시지가 표시된다.
         self.check_for_row_in_list_table('1: 우유 사기')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, '빈 아이템을 등록할 수 없습니다.')
 
         # 아이템을 입력하면 정상 동작한다.
@@ -46,5 +46,23 @@ class ItemValidationTest(FunctionalTest):
 
         # 도움이 되는 에러 메시지를 본다.
         self.check_for_row_in_list_table('1: 콜라 사기')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, '이미 등록한 작업입니다.')
+
+    def test_error_messages_are_cleared_on_input(self):
+        # 에디스는 검증에러를 발생시키도록 신규 목록을 시작한다.
+        self.browser.get(self.server_url)
+        self.get_item_inpu_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # 에러를 제거하기 위해 입력 상자에 타이핑하기를 시작한다.
+        self.get_item_inpu_box().send_keys('a')
+
+        # 에러 메시지가 사라진 것을 보고 기뻐한다.
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
