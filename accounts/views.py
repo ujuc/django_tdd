8c1af54f41ae466d24ea42b5a7cddd151611e9f4
Sungjin Kang <ujuc@ujuc.kr>
2015-05-05 02:11:46 +0900

    p338

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import httpResponse
 
-# Create your views here.
+def persona_login():
+	authenticate(assertion=request.POST['assertion'])
+	return httpResponse()
--- superlists/urls.py
+++ superlists/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
+    url(r'^accounts/', include('accounts.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
