ed749a273a0b0b5de41944a13bb4219d1657040c
Sungjin Kang <ujuc@ujuc.kr>
2015-05-05 18:30:56 +0900

    16장 완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-	url(r'^login$', 'accounts.views.persona_login', name='persona_login'),
+                       url(r'^login$', 'accounts.views.persona_login', name='persona_login'),
+                       url(r'^logout$', 'django.contrib.auth.views.logout',
+                           {'next_page': '/'}, name='logout'),
 )
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -29,9 +29,20 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # 로그인된 것을 알 수 있다.
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
+        self.wait_to_be_logged_in()
+
+        # 페이지를 새로고침해서 실제 세션 로그인 상태인 것을 확인한다
+        # 일회성 로구인이 아니다
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # 새로운 기능에 겁을 먹었다. 반사적으로 '로그아웃'을 클릭한다.
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # 새로고침 후에 '로그아웃' 상태가 계속된다.
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
 
     def switch_to_new_window(self, text_in_title):
         retries = 60
@@ -46,5 +57,19 @@ class LoginTest(FunctionalTest):
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was {}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
         )
+
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_logoin')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
